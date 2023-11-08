a057c7e1f4183ee1697814b8ad814feb132bb7c3
JamesPrideauxGhee <146222783+JamesPrideauxGhee@users.noreply.github.com>
2023-11-08 15:06:11 +0000

    Update and rename plot_convergence_2.py to plot_convergence.py

--- data/plot_convergence.py
+++ data/plot_convergence.py
@@ -2,6 +2,8 @@ import numpy as np
 import pyccl as ccl
 import matplotlib.pyplot as plt
 
+## File used to make the plot of the convergence of the unconstrained contribution
+
 
 cosmo =  ccl.Cosmology(h = 0.6774, Omega_b = 0.04865, Omega_c = 0.3089, n_s = 0.9667, sigma8 = 0.8159)
 
@@ -18,6 +20,7 @@ kernel[1] = ccl.growth_factor(cosmo,(1./(1+z_range))) * (1 + z_range)
 custom = ccl.Tracer()
 custom.add_tracer(cosmo, kernel=kernel, der_angles=0, der_bessel=0)
 
+# Test a range of max ls
 max_ells = [500000,1000000,1500000,2000000,2500000,3000000,3500000,4000000,4500000,5000000,6000000,7000000,8000000]
 dm_uncon_2 = np.zeros((len(max_ells)))
 for i in range(len(max_ells)):
@@ -36,15 +39,13 @@ for i in range(len(max_ells)):
     cls = ccl.angular_cl(cosmo,custom,custom,ells, p_of_k_a = pk_cut)
     # Calculate the cls for this case
     dm_uncon_2[i] = np.sum( ( (2*ells + 1) * cls) / (4*np.pi) )
-    other = ccl.correlations.correlation(cosmo,ells,cls,theta=0,method='Bessel')
-    print(f'sum cls = {dm_uncon_2[i]}, other = {other}')
 
 # Plot the results
 plt.figure()
 plt.plot(max_ells,dm_uncon_2,'+')
 plt.xlabel('max l')
 plt.ylabel('dm_uncon_2')
-plt.savefig('l_cl_convergence_2.png')
+plt.savefig('l_cl_convergence.png')
 
 # Save results
 np.savez('dm_uncon_convergence',max_ells=max_ells,dm_uncon_2=dm_uncon_2)
